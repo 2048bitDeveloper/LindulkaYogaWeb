f6206527ae6ca2aa7edb97d72344f97467382c5d
Jan Donath <donath.jan@gmail.com>
2022-11-06 16:58:15 +0100

    major commit with swiper, css made modular

---
+++ kdeMeNajdete.js
@@ -0,0 +1,47 @@
+let kdeMeNajdete = [
+    {
+        headline: "Hatha Yoga",
+        description: "Hatha yoga is the branch of yoga that typically comes to mind when you thing of yoga in general terms. With Hatha yoga there is no need fo you to feel like your life is out of control. You learn to direct and master your energy. The practice involves breath, body and mind and classess are usually 45 minutes to 90 minutes of breathing, yoga poses and meditation",
+        days: [
+            {
+                day: "Weekly on Tuesdays",
+                times: [
+                    {time: "9:00 - 10:20"},
+                    {time: "12:00 - 13:20"}
+                ]    
+            },
+            {
+                day: "Weekly on Thursdays",
+                times: [
+                    {time: "9:00 - 10:20"},
+                    {time: "12:00 - 13:20"}
+                ]
+            }
+        ],
+        image: "/assets/achievements-img-v2.png"
+    },
+    {
+        headline: "Jin Yoga",
+        description: "Hatha yoga is the branch of yoga that typically comes to mind when you thing of yoga in general terms. With Hatha yoga there is no need fo you to feel like your life is out of control. You learn to direct and master your energy. The practice involves breath, body and mind and classess are usually 45 minutes to 90 minutes of breathing, yoga poses and meditation",
+        days: [
+            {
+                day: "Weekly on Tuesdays",
+                times: [
+                    {time: "9:00 - 10:20"},
+                    {time: "12:00 - 13:20"}
+                ]    
+            },
+            {
+                day: "Weekly on Thursdays",
+                times: [
+                    {time: "9:00 - 10:20"},
+                    {time: "12:00 - 13:20"}
+                ]
+            }
+        ],
+        image: "/assets/achievements-img-v2.png"
+    }
+]
+
+
+// console.log(kdeMeNajdete[0].days[0].times[0].time)
--- script.js
+++ script.js
@@ -1,74 +1,170 @@
-
 // SHOW/HIDE NAVBAR HAMBURGER MENU
 
-var hamburger = document.getElementById("hamburger")
-var navbarUl = document.getElementById("navbar-ul")
-var navbar = document.getElementById("navbar")
-var buttonTelephone = document.getElementById("button-telephone")
-var headerId = document.getElementById("header")
-var headerContainer = document.getElementById("header-container")
-var buttonPhoneNumber = document.getElementById("button-phone-number")
+var hamburger = document.getElementById("hamburger");
+var navbarUl = document.getElementById("navbar-ul");
+var navbar = document.getElementById("navbar");
+var buttonTelephone = document.getElementById("button-telephone");
+var headerId = document.getElementById("header");
+var headerContainer = document.getElementById("header-container");
+var buttonPhoneNumber = document.getElementById("button-phone-number");
 
-var phoneSize = window.matchMedia("(max-width: 790px)")
+var phoneSize = window.matchMedia("(max-width: 790px)");
 
-
-
-hamburger.onclick = function() {
-
+hamburger.onclick = function () {
     if (navbar.style.display === "block") {
-        navbar.style.display = "none"
-
+        navbar.style.display = "none";
     } else {
-        navbar.style.display = "block"
+        navbar.style.display = "block";
     }
-}
+};
 
-navbarUl.onclick = function() {
-
+navbarUl.onclick = function () {
     if (phoneSize.matches) {
-      navbar.style.display = "none"
+        navbar.style.display = "none";
     }
-}
-
-
-
-
+};
 
 // ON SCROLL SHOW/HIDE/SCALE HAMBURGER NAVBAR AND SOCIAL
 
-var previousScrollposition = window.pageYOffset
-var socialContainer = document.getElementById("social-container")
+var previousScrollposition = window.pageYOffset;
+var socialContainer = document.getElementById("social-container");
 
+window.onscroll = function () {
+    var currentScrollPosition = window.pageYOffset;
 
-window.onscroll = function() {
-  var currentScrollPosition = window.pageYOffset
+    if (
+        document.body.scrollTop > 250 ||
+        document.documentElement.scrollTop > 250
+    ) {
+        if (previousScrollposition > currentScrollPosition) {
+            hamburger.style.top = "-3rem";
+            socialContainer.style.right = "2.6%";
 
-  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-    
-    if (previousScrollposition > currentScrollPosition) {
+            // Desktop size scaling navbar
+            headerContainer.style.transform = "scale(1)";
+            headerContainer.style.paddingTop = "2rem";
+            buttonPhoneNumber.style.opacity = "1";
+        } else {
+            hamburger.style.top = "-20vh";
+            socialContainer.style.right = "-10vw";
 
-        hamburger.style.top = "-4rem"
-        socialContainer.style.right = "2.6%"
-        
-        // Desktop size scaling navbar
-        headerContainer.style.transform = "scale(1)"
-        headerContainer.style.paddingTop = "5rem"
-        buttonPhoneNumber.style.opacity = "1"
-
-      } else {
-        hamburger.style.top = "-15rem"
-        socialContainer.style.right = "-6rem"
-        
-        // Desktop size scaling navbar
-       if (!phoneSize.matches) {
-          headerContainer.style.transform = "scale(0.8)"
-          headerContainer.style.paddingTop = "2rem"
-          buttonPhoneNumber.style.opacity = "0"
+            // Desktop size scaling navbar
+            if (!phoneSize.matches) {
+                headerContainer.style.transform = "scale(0.8)";
+                headerContainer.style.paddingTop = "2rem";
+                buttonPhoneNumber.style.opacity = "0";
+            }
         }
-        
+        previousScrollposition = currentScrollPosition;
     }
-    previousScrollposition = currentScrollPosition
+};
 
-  }
+// ┌─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=┐
+// │***        SLIDE SHOW START          ***│
+// └=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─┘
 
+// ┌─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=┐
+// │***          SWIPER START            ***│
+// └=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─┘
+
+let menu = ["Hatha yoga", "Jin yoga", "Aero yoga"];
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (menu[index]) + '</span>'}
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    initialSlide: 1
+});
+
+// ┌─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=┐
+// │***          SWIPER ENDS             ***│
+// └=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─┘
+
+// let slideIndex = 2;
+
+// function plusSlides(n) {
+//     showSlides((slideIndex += n));
+// }
+
+// function currentSlide(n) {
+//     showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//     let slides = document.getElementsByClassName("places-content");
+//     let pills = document.getElementsByClassName("pill");
+//     // Make all disappear.
+//     if (n > slides.length) {
+//         slideIndex = 1;
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length;
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     // Pill in navigation changing color.
+//     for (i = 0; i < pills.length; i++) {
+//         pills[i].className = pills[i].className.replace(" active", "");
+//     }
+
+//     slides[slideIndex - 1].style.display = "grid";
+//     pills[slideIndex - 1].className += " active";
+// }
+
+// showSlides(slideIndex);
+
+// ┌─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=┐
+// │***        SLIDE SHOW ENDS           ***│
+// └=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─┘
+
+// ┌─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=┐
+// │***           MODAL START            ***│
+// └=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─┘
+
+let modal = document.getElementById("modal");
+let modalButton = document.getElementById("modal-button");
+
+modalButton.addEventListener("click", toggleModal);
+
+function hideModal() {
+    modal.style.display = "none";
+}
+
+function toggleModal() {
+    // modal.style.display = "grid"
+
+    if (modal.style.display === "none") {
+        modal.style.display = "grid";
+    } else {
+        modal.style.display = "grid";
+    }
+}
+
+// ┌─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=┐
+// │***           MODAL ENDS             ***│
+// └=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─=─┘
+
+const today = new Day();
+const timeInFuture = new Day(22, 11, 20)
+
+// let main = document.getElementsByTagName("main");
+
+if (today > timeInFuture) {
+    modalButton.style.display = "none";
 }
